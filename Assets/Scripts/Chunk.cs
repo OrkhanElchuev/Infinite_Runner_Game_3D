2bb02ba342eb0890c91dc0f630e15dc354794eaa
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-01-14 23:51:23 +0100

    Complete the logic of auto-generating fences on lanes

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
@@ -5,7 +6,6 @@ public class Chunk : MonoBehaviour
     [SerializeField] GameObject fencePrefab;
     [SerializeField] float[] lanes = {-2.5f, 0f, 2.5f};
     
-
     void Start()
     {
         SpawnFence();
@@ -13,8 +13,21 @@ public class Chunk : MonoBehaviour
 
     void SpawnFence()
     {
-        int randomLaneIndex = Random.Range(0, lanes.Length);
-        Vector3 spawnPosition = new Vector3(lanes[randomLaneIndex], transform.position.y, transform.position.z);
-        Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        List<int> availableLanes = new List<int> {0, 1, 2};
+        int fencesToSpawn = Random.Range(0, lanes.Length);
+        // Make sure there is always at least 1 available lane for a player
+        for (int i = 0; i < fencesToSpawn; i++)
+        {
+            if (availableLanes.Count <= 0) break;
+
+            // Find and select an available lane without an already instantiated fence
+            int randomLaneIndex = Random.Range(0, availableLanes.Count);
+            int selectedLane = availableLanes[randomLaneIndex];
+            // Remove it from the list to not instantiate another fence on the same lane
+            availableLanes.RemoveAt(randomLaneIndex);
+
+            Vector3 spawnPosition = new Vector3(lanes[selectedLane], transform.position.y, transform.position.z);
+            Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        }
     }
 }
