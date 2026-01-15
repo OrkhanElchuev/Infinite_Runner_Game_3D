b2b539314be546dfc5caaf55453bce9eb72f76e8
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-01-15 16:38:20 +0100

    Add Particle Effect when Speeding up

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] ParticleSystem speedUpParticleSystem;
     [SerializeField] float minFOV = 30f;
     [SerializeField] float maxFOV = 120f;
     [SerializeField] float zoomDuration = 1f;
+    [SerializeField] float zoomSpeedModifier = 5f;
 
     CinemachineCamera cinemachineCamera;
 
@@ -18,13 +20,20 @@ public class CameraController : MonoBehaviour
     // Field of View 
     public void ChangeCameraFOV(float speedAmount)
     {
+        StopAllCoroutines();
         StartCoroutine(ChangeFOVRoutine(speedAmount));
+
+        // Show Particles only when speeding up
+        if (speedAmount > 0)
+        {
+            speedUpParticleSystem.Play();
+        }
     }
 
     IEnumerator ChangeFOVRoutine(float speedAmount)
     {
         float startFOV = cinemachineCamera.Lens.FieldOfView;
-        float targetFOV = Mathf.Clamp(startFOV + speedAmount, minFOV, maxFOV);
+        float targetFOV = Mathf.Clamp(startFOV + speedAmount * zoomSpeedModifier, minFOV, maxFOV);
 
         float elapsedTime = 0f;
         // Lerp slowly between point A to point B
