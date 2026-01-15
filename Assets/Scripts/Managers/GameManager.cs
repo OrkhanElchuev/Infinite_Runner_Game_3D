7cb9211e698b7d9490ca8a5f5097b36924e03f28
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-01-15 20:22:55 +0100

    Add Game Over text with slow motion effect

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] TMP_Text timeText;
+    [SerializeField] GameObject gameOverText;
+    
+    [Header("Settings")]
     [SerializeField] float startTime = 5f;
 
     float timeLeft;
@@ -18,5 +22,17 @@ public class GameManager : MonoBehaviour
         timeLeft -= Time.deltaTime;
         // F1 for 1 decimal place formatting
         timeText.text = timeLeft.ToString("F1");
+
+        if (timeLeft <= 0f)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        gameOverText.SetActive(true);
+        // Create a Slow motion effect
+        Time.timeScale = 0.1f;
     }
 }
